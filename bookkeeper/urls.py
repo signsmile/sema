fd83bb8d16c53857c19e140ec5698888dd1bf6cc
signsmile <signsmile@163.com>
2020-01-07 22:18:56 +0800

    add some views

--- bookkeeper/urls.py
+++ bookkeeper/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('account/<int:account_id>/', views.account, name='Account'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('account/<int:pk>/', views.AccountView.as_view(), name='account'),
+    path('transaction/add',
+         views.TransactionCreateView.as_view(),
+         name='add_transaction'),
+    path('accountentry/add',
+         views.AccountEntryCreateView.as_view(),
+         name='add_accountentry'),
 ]
--- bookkeeper/views.py
+++ bookkeeper/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
-from .models import Account, AccountEntry
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from .models import Account, AccountEntry, Transaction
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(ListView):
+    model = Account
+    context_object_name = 'account_list'
 
 
-def account(request, account_id):
-    account = Account.objects.get(accid=account_id)
-    entry_list = AccountEntry.objects.filter(account=account_id)
-    print(account)
-    print(entry_list)
-    context = {'entry_list': entry_list, 'balance': account.balance()}
-    return render(request, 'bookkeeper/account.html', context)
+class AccountView(DetailView):
+    model = Account
+    context_object_name = 'account'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['entries'] = self.object.entries.all()
+    #     return context
+
+class TransactionCreateView(CreateView):
+    model = Transaction
+    fields = ['t_stamp', 'description', 'project']
+
+
+class AccountEntryCreateView(CreateView):
+    model = AccountEntry
+    fields = ['transaction', 'account', 'amount', 'description']
